9ee96dd354d1e6e76493a35b85a945557a0667f4
AZ000ZA <150498639+az000za@users.noreply.github.com>
2023-12-22 18:38:22 -0500

    Update PathFinder.js

--- PathFinder.js
+++ PathFinder.js
@@ -1,13 +1,18 @@
 class PathFinder {
-  constructor(){
-    
+  constructor(initState, methods){
+    this.state = initState; 
+    this.methods = methods;    
   }
   paths = [];
   state = {
     // properties to have methods modify
   };
-  methods = {};
-  goto(targetState){
-    
+  methods = {
+    // These are the methods to get populated by the codebase
+  };
+  goto(targetState, fromState, avoidState){
+    // targetState is the end goal ... the destination.
+    // fromState is the starting point status
+    // avoidState means it's not allowed to have certain values
   }
 }
